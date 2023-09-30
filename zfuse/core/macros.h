fc81ef3c3d767e259533b0e824940bb28858b7dd
paging <paging@DESKTOP-A5QCNE5>
2023-09-30 13:20:17 -0500

    Array macro and Crc32

---
+++ zfuse/core/macros.h
@@ -0,0 +1,12 @@
+/*
+* Zel Software, SP
+* Last modified: 09/30/2023
+* This file is part of zfuse, licensed under the MIT License (MIT).
+* See the LICENSE file in the project root for license information.
+*/
+#pragma once
+
+#include <cstddef>
+
+// Macro to get the size of an array
+#define ZFUSE_ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
